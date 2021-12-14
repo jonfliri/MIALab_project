69c05e86a23e1489c12dfff37c6d5ec13ecf687e
jonfliri <jon.fliri@hotmail.de>
2021-12-14 16:57:05 +0100

    new

--- bin/mia-result/Stat/Cal.R
+++ bin/mia-result/Stat/Cal.R
@@ -1,1 +1,22 @@
-my_data <- read.csv(Solution.choose())
+setwd("C:/Code/MIALab_project/bin/mia-result/Stat") #set my working directory
+data <- read.csv(file="Test.csv", header=TRUE)
+data
+data$X <- ordered(data$X,levels = c("ctrl", "test"))
+
+library(dplyr)
+group_by(data, X) %>%
+  summarise(
+    count = n(),
+    mean = mean(STD, na.rm = TRUE),
+    sd = sd(STD, na.rm = TRUE),
+    median = median(STD, na.rm = TRUE),
+    IQR = IQR(STD, na.rm = TRUE)
+  )
+
+library("ggpubr")
+ggboxplot(data, x = "X", y = "STD", color = "X", palette = c("#00AFBB", "#E7B800"),order = c("ctrl", "test"),ylab = "Weight", xlab = "Treatment")
+
+library("ggpubr")
+ggline(data, x = "X", y = "STD", add = c("mean_se", "jitter"), order = c("ctrl", "test"),ylab = "Weight", xlab = "Treatment")
+
+kruskal.test(STD ~ X, data = data)
